5cf488db5f9e7d416d25a2148910edd721ee45fc
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-07-26 03:18:54 +0300

    add what we do described

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 $(document).ready(function() {
-    $("form#formADD").submit(function(event) {
-            var name = $("input#MERGE1").val();
-            var email = $("input#MERGE0").val();
-            var message = $("textarea#comment").val();
-            if ($("input#MERGE1").val() && $("input#MERGE0"))) {
-            alert(name + ", we have received your message. How can we assist you.");
-        } else {
-            alert("Please enter your name and email!");
-        }
-    });
-
-});
-$(document).ready(function() {
     $("#development-image").click(function() {
         $("#development-image").slideDown('1500').hide('1000');
         $("#development").show('1500');
@@ -94,4 +81,19 @@ $(document).ready(function() {
     }).mouseout(function() {
         $("#overlay8").hide();
     });
+});
+$(document).ready(function() {
+    $("form#form34A").submit(function(event) {
+        // event.preventDefault();
+        var name = $("input#MERGE1").val();
+        var email = $("input#MERGE0").val();
+        var message = $("textarea#comment").val();
+        if ($("input#MERGE1").val() && $("input#MERGE0").val()) {
+            alert(name + ", we have received your message. Thank you for reaching out to us.");
+        } else {
+            alert("Please enter your name and email!");
+        }
+
+    });
+
 });
